2c11e287d844a5226ac0fcb4cc5fc6c03fcdc9d3
welcome-to-the-sunny-side <nonadhocproblems@gmail.com>
2024-11-04 20:23:49 +0530

    updated something

--- misc/Debug.cpp
+++ misc/Debug.cpp
@@ -1,15 +1,13 @@
 template<typename T> 
 void __print(const T& x) {
-    cerr << '{';
     if constexpr (requires { x.size(); } && !requires { x.length(); })
         for (auto i : x) __print(i), cerr << ',';
     else if constexpr (requires { x.first; })
         __print(x.first), cerr << ',', __print(x.second);
     else    cerr << x;
-    cerr << '}';
 }
 
 template <typename... Args>
-void _print(const Args&... args) {((__print(args), cerr << ", "), ...);}
+void _print(const Args&... args) {((cerr << '{', __print(args), cerr << "}, "), ...);}
 
 #define debug(...) cerr << "[" << #__VA_ARGS__ << "] = ["; _print(__VA_ARGS__); cerr << "]\n";
