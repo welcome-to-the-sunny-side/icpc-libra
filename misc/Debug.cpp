1a148b85bbaa9755cd10b46333a5f1bd5989048b
welcome-to-the-sunny-side <nonadhocproblems@gmail.com>
2024-11-04 17:39:10 +0530

    updated something

--- misc/Debug.cpp
+++ misc/Debug.cpp
@@ -1,48 +1,17 @@
-// Integral Types
-template<typename T>
-requires is_arithmetic_v<T>
-void __print(const T& x) { cerr << x; }
+template<typename T> 
+void __print(const T& x) {cerr << x;}  // handles all numeric types and strings
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
 
-// Char
-void __print(char x) { cerr << '\'' << x << '\''; }
-
-// String
-void __print(const string& x) { cerr << '\"' << x << '\"'; }
-
-// Boolean
-void __print(const bool& x) { cerr << (x ? "true" : "false"); }
-
-// Handle pairs
 template<typename T, typename V>
-void __print(const pair<T, V>& x) {
-    cerr << '{';
-    __print(x.first);
-    cerr << ',';
-    __print(x.second);
-    cerr << '}';
-}
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
 
-// Handle any iterable container
 template<typename T>
-requires (!is_arithmetic_v<T> && requires(T x) { begin(x); end(x); })
-void __print(const T& x) {
-    int f = 0;
-    cerr << '{';
-    for (const auto& i : x) {
-        cerr << (f++ ? "," : "");
-        __print(i);
-    }
-    cerr << "}";
-}
+void __print(const vector<T> &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
 
-// Variadic template for multiple arguments
-template<typename T, typename... V>
-void _print(T t, V... v) {
-    __print(t);
-    if (sizeof...(v)) cerr << ", ";
-    if constexpr (sizeof...(v) > 0) {
-        _print(v...);
-    }
-}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
 
-#define debug(x...) { cerr << "[" << #x << "] = ["; _print(x); cerr << "]\n"; }
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
