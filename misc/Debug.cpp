c86e25caf22d2a589b565431d0b28cbffd56aeca
welcome-to-the-sunny-side <nonadhocproblems@gmail.com>
2024-11-04 19:38:26 +0530

    updated something

--- misc/Debug.cpp
+++ misc/Debug.cpp
@@ -1,29 +1,23 @@
-class dbg
-{
-public:
-    template<typename T> 
-    void __print(const T& x) {cerr << x;}
-
-    template<typename T, typename V>
-    void __pair_print(const pair<T, V> &x) {cerr << '{'; __handle(x.first); cerr << ','; __handle(x.second); cerr << '}';}
-
-    template<typename T>
-    void __iterate_print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __handle(i); cerr << "}";}
-
-    template<typename T>
-    void __handle(const T &t) {
-        if constexpr (requires {t.size();} and !is_same_v<T, string>)   __iterate_print(t);
-        else if constexpr (requires {t.first;})    __pair_print(t);
-        else    __print(t);
+template<typename T> 
+void __print(const T& x) {
+    cerr << '{';
+    if constexpr (requires {x.size();} and !is_same_v<T, string>){
+        int f = 0;
+        for(auto i : x) cerr << (f ++ ? "," : ""), __print(i);
     }
+    else if constexpr (requires {x.first;})
+        __print(x.first), cerr << ',', __print(x.second);
+    else
+        cerr << x;
+    cerr << '}';
+}
 
-    void _print() {cerr << "]\n";}
-    template <typename T, typename... V>
-    void _print(T t, V... v) {
-        __handle(t);
-        if (sizeof...(v)) cerr << ", "; 
-        _print(v...);
-    }
-}debugger;
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {
+    __print(t);
+    if (sizeof...(v)) cerr << ", "; 
+    _print(v...);
+}
 
-#define debug(x...) cerr << "[" << #x << "] = ["; debugger._print(x)
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
