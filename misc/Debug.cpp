536c678b69045b491de1c070e79d0dcab72d6482
welcome-to-the-sunny-side <nonadhocproblems@gmail.com>
2024-11-04 17:33:55 +0530

    updated something

--- misc/Debug.cpp
+++ misc/Debug.cpp
@@ -7,42 +7,42 @@ void __print(const T& x) { cerr << x; }
 void __print(char x) { cerr << '\'' << x << '\''; }
 
 // String
-void __print(const std::string& x) { cerr << '\"' << x << '\"'; }
+void __print(const string& x) { cerr << '\"' << x << '\"'; }
 
 // Boolean
 void __print(const bool& x) { cerr << (x ? "true" : "false"); }
 
 // Handle pairs
 template<typename T, typename V>
-void __print(const std::pair<T, V>& x) {
-    std::cerr << '{';
+void __print(const pair<T, V>& x) {
+    cerr << '{';
     __print(x.first);
-    std::cerr << ',';
+    cerr << ',';
     __print(x.second);
-    std::cerr << '}';
+    cerr << '}';
 }
 
 // Handle any iterable container
 template<typename T>
-requires (!std::is_arithmetic_v<T> && requires(T x) { std::begin(x); std::end(x); })
+requires (!is_arithmetic_v<T> && requires(T x) { begin(x); end(x); })
 void __print(const T& x) {
     int f = 0;
-    std::cerr << '{';
+    cerr << '{';
     for (const auto& i : x) {
-        std::cerr << (f++ ? "," : "");
+        cerr << (f++ ? "," : "");
         __print(i);
     }
-    std::cerr << "}";
+    cerr << "}";
 }
 
 // Variadic template for multiple arguments
 template<typename T, typename... V>
 void _print(T t, V... v) {
     __print(t);
-    if (sizeof...(v)) std::cerr << ", ";
+    if (sizeof...(v)) cerr << ", ";
     if constexpr (sizeof...(v) > 0) {
         _print(v...);
     }
 }
 
-#define debug(x...) { std::cerr << "[" << #x << "] = ["; _print(x); std::cerr << "]\n"; }
+#define debug(x...) { cerr << "[" << #x << "] = ["; _print(x); cerr << "]\n"; }
