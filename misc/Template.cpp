a21536a0c51a94f7234eddff1c355432f1b9dec5
unalive <nonadhocproblems@gmail.com>
2024-12-24 19:38:19 +0530

    update something

--- misc/Template.cpp
+++ misc/Template.cpp
@@ -1,10 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 #pragma GCC optimize("O3,unroll-loops")
 #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 
 #define endl "\n"
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 signed main()
 {
